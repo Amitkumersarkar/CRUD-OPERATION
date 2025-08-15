231976e36e888b26fb6f896446a56876f6d745fd
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-15 23:20:06 +0600

    added form

--- simple-crud-client-side/src/Components/ClientSide.jsx
+++ simple-crud-client-side/src/Components/ClientSide.jsx
@@ -1,9 +1,23 @@
 
 const ClientSide = () => {
-
+    const handleAddUser = (e) => {
+        e.preventDefault();
+        const form = e.target.from;
+        const name = form.name.value;
+        const email = form.email.value;
+        const user = { name, email };
+        console.log(user);
+    }
     return (
         <div className="text-center mt-10">
             <h2>Simple CRUD</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" id="" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="submit" value="add users" />
+            </form>
         </div>
     );
 };
