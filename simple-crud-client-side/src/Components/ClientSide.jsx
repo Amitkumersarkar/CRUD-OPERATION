265008010dae9aa298df1d227ba6ae407f44b6d6
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-15 23:24:29 +0600

    updated code

--- simple-crud-client-side/src/Components/ClientSide.jsx
+++ simple-crud-client-side/src/Components/ClientSide.jsx
@@ -2,7 +2,7 @@
 const ClientSide = () => {
     const handleAddUser = (e) => {
         e.preventDefault();
-        const form = e.target.from;
+        const form = e.target;
         const name = form.name.value;
         const email = form.email.value;
         const user = { name, email };
@@ -10,13 +10,27 @@ const ClientSide = () => {
     }
     return (
         <div className="text-center mt-10">
-            <h2>Simple CRUD</h2>
-            <form onSubmit={handleAddUser}>
-                <input type="text" name="name" id="" />
+            <h2>SIMPLE CRUD</h2>
+            <form onSubmit={handleAddUser} className="space-y-4 mt-5">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter name"
+                    className="border border-gray-400 px-3 py-2 rounded"
+                />
                 <br />
-                <input type="email" name="email" id="" />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter email"
+                    className="border border-gray-400 px-3 py-2 rounded"
+                />
                 <br />
-                <input type="submit" value="add users" />
+                <input
+                    type="submit"
+                    value="Add user"
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 cursor-pointer"
+                />
             </form>
         </div>
     );
