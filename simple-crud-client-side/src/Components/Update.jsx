a9233095a0d1e080081ce263a05c7bde5d0fcd5a
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-16 21:28:38 +0600

    added client side updated method applied

--- simple-crud-client-side/src/Components/Update.jsx
+++ simple-crud-client-side/src/Components/Update.jsx
@@ -7,7 +7,24 @@ const Update = () => {
         const form = e.target;
         const name = form.name.value;
         const email = form.name.value;
-        console.log(name, email)
+        console.log(name, email);
+        const updatedUser = { name, email };
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+
+            body: JSON.stringify(updatedUser)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    alert('user updated successfully');
+                }
+            })
     }
     return (
         <div className="text-center font-bold mt-10">
