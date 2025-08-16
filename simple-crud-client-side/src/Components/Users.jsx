a39d0c0948a7ae751606be12a5a4e5327e4399eb
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-16 18:00:38 +0600

    updated code

--- simple-crud-client-side/src/Components/Users.jsx
+++ simple-crud-client-side/src/Components/Users.jsx
@@ -3,12 +3,27 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
     const users = useLoaderData();
 
+    const handleDelete = (_id) => {
+        console.log('deleted ', _id);
+
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    alert('deleted successfully');
+                }
+            })
+    }
+
     return (
-        <div>
+        <div className="text-center mt-10">
             <h2>{users.length}</h2>
             <div>
                 {
-                    users.map((user) => <p key={user._id}>{user.name}:{user.email}</p>)
+                    users.map((user) => <p key={user._id}> {user.name}:{user.email} : {user._id}  <button className="btn btn-ghost font-bold " onClick={() => handleDelete(user._id)}>X</button></p>)
                 }
             </div>
         </div>
--- simple-crud-client-side/src/main.jsx
+++ simple-crud-client-side/src/main.jsx
@@ -11,15 +11,12 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <ClientSide></ClientSide>,
-    children: [
-     
-      {
-        path: '/users',
-        element: <Users></Users>,
-        loader:()=>fetch('http://localhost:5000/users')
-      }
-    ]
   },
+  {
+    path: '/users',
+    element: <Users></Users>,
+    loader: () => fetch('http://localhost:5000/users')
+  }
 ]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
